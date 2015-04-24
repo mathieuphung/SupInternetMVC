c8df65592562760c64c65ada291fce5908717eb5
mathieuphung <phung.mathieu@gmail.com>
2015-04-24 14:47:24 +0200

    first commit

--- web/index.php
+++ web/index.php
@@ -1,8 +1,10 @@
-<?php
 
+<?php
 /**
  * This is you FrontController, the only point of access to your webapp
  */
+
+require __DIR__ . '/../vendor/autoload.php';
 
 /**
  * Use Yaml components for load a config routing, $routes is in yaml app/config/routing.yml :
@@ -11,25 +13,18 @@
  *
  *
  */
-$routes = ...
-
+$routes = ...;
 //ControllerClassName, end name is ...Controller
 $controller_class = ... ;
-
 //ActionName, end name is ...Action
 $action_name = ...;
-
 $controller = new $controller_class();
-
 //$Request can by an object
 $request['request'] = &$_POST;
 $request['query'] = &$_GET;
 //...
-
 //$response can be an object
 $response = $controller->$action_name($request);
-
-
 /**
  * Use Twig !
  */
